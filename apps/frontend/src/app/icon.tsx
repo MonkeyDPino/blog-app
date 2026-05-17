868c5259d5b12ac85e3e70adea89bf345b9ea859
Juan Pino <juanrespolo@gmail.com>
2026-05-16 19:04:21 -0500

    style: apply lint fixes to favicon icon

--- apps/frontend/src/app/icon.tsx
+++ apps/frontend/src/app/icon.tsx
@@ -5,31 +5,29 @@ export const contentType = 'image/png';
 
 export default function Icon() {
   return new ImageResponse(
-    (
-      <div
+    <div
+      style={{
+        width: 32,
+        height: 32,
+        borderRadius: 8,
+        background: '#818CF8',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <span
         style={{
-          width: 32,
-          height: 32,
-          borderRadius: 8,
-          background: '#818CF8',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
+          color: '#1E1B4B',
+          fontSize: 20,
+          fontWeight: 700,
+          lineHeight: 1,
+          fontFamily: 'serif',
         }}
       >
-        <span
-          style={{
-            color: '#1E1B4B',
-            fontSize: 20,
-            fontWeight: 700,
-            lineHeight: 1,
-            fontFamily: 'serif',
-          }}
-        >
-          P
-        </span>
-      </div>
-    ),
+        P
+      </span>
+    </div>,
     { ...size },
   );
 }
